43f3d13871f2092b2dad41f63a1e71afb1cbf867
Ajay Singh <contact@ajaysingh.ca>
2023-06-19 21:26:56 -0400

    started new server with file setup, mongoDB connected and working with basic middleware

---
+++ Index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 9001;
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public')); // serve static files
+
+// use async await to connect to MongoDB instead of callback
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connected to MongoDB...');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+  }
+}
+
+connectToMongoDB();
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
